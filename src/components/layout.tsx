8934e88fa8d39d4a5804e2c8e814000715a4780b
Samuel Seng <djsamseng@gmail.com>
2022-02-20 20:10:40 -0500

    Add logo

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Link, useStaticQuery, graphql } from "gatsby";
+import { Link, useStaticQuery, graphql, } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 
 import NavBar from "./navbar";
 import SearchBar from "./searchbar";
@@ -20,10 +21,14 @@ const TitleComponent = () => {
       }
     }
   `);
+  // CC0 license https://www.svgrepo.com/svg/252816/badger
   return (
-    <div className="font-bold font-heading text-3xl">
+    <div className="font-bold font-heading text-3xl flex-1">
       <p className="sr-only">{data.site.siteMetadata.title}</p>
-      <Link to="/">{data.site.siteMetadata.title}</Link>
+      <Link to="/">
+        <StaticImage className="mt-[-30px] mb-[-40px] mr-5" height={100} alt="Gather Badger" src="../images/badger-logo.svg"/>
+        {data.site.siteMetadata.title}
+      </Link>
     </div>
   );
   // <header className="text-3xl font-bold font-heading">{data.site.siteMetadata.title}</header>
@@ -59,7 +64,7 @@ class ControlsPanel extends React.Component<ControlsPanelProps, ControlsPanelSta
       :
       (<svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"></path></svg>);
     return (
-      <div className="flex mr-6 items-center">
+      <div className="flex-1 mr-6 flex flex-col items-end">
         <button className="" onClick={this.onToggleDarkTheme.bind(this)}>
           {icon}
         </button>
@@ -82,8 +87,8 @@ class HeaderComponent extends React.Component<HeaderComponentProps, HeaderCompon
 
   public render() {
     return (
-      <header className="bg-gray-100 border-b dark:bg-slate-800 dark:border-gray-700 lg:w-full lg:top-0 lg:left-0">
-        <section className="container px-4 py-5 mx-auto">
+      <header className="bg-gray-100 border-b dark:bg-slate-800 dark:border-gray-700 lg:w-full lg:top-0 lg:left-0 mb-5">
+        <section className="px-2 py-5 mx-5 flex flex-col">
           <div className="flex flex-row justify-between">
               <TitleComponent />
               <NavBar />
